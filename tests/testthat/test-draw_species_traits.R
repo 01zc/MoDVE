bfba374d4080ac5f97e0d195604138144344bb6d
Theo Pannetier <theo@pannetier.nl>
2026-01-07 17:30:31 +0100

    wip unit tests for species traits generation

--- tests/testthat/test-draw_species_traits.R
+++ tests/testthat/test-draw_species_traits.R
@@ -33,7 +33,61 @@ test_that("Input is checked correctly", {
 
 })
 
-test_that("Input is checked correctly", {
+test_that("Species traits meet requirements", {
+
   sp_params <- ref_sp_params
-  draw_species_traits(sp_params)
+  species_traits <- draw_species_traits(sp_params)
+
+  # All expected elements are present
+  expect_equal(names(species_traits), species_trait_names())
+
+  list2env(species_traits, envir = environment())
+  expect_gte(MassAtMaturity, 0)
+  expect_gte(MaximumMass, MassAtMaturity)
+  expect_gte(GrowthRate, 0)
+
+  expect_gte(DispersalKernel, 0)
+  expect_gte(DispersalKernelAsymmetry, 0)
+  expect_lte(DispersalKernelAsymmetry, 1)
+
+  expect_gte(RecruitmentInvestmentRel, 0)
+  expect_lte(RecruitmentInvestmentRel, 1)
+  expect_gte(RecruitmentInc, 0)
+
+  expect_gte(MinLight, 0)
+  expect_gte(OptimumLight, MinLight)
+  expect_gte(MaxLight, OptimumLight)
+  expect_equal(LightBreadth, MaxLight - MinLight)
+
+  # Light parameters satisfy the parabolic response equation
+  light_reponse_params <- get_light_resp_params( # TODO: is this function correct?
+    MinLight, MaxLight, OptimumLight
+    )
+  expect_equal(LightResponseA, light_reponse_params[1])
+  expect_equal(LightResponseB, light_reponse_params[2])
+  expect_equal(LightResponseC, light_reponse_params[3])
+
+  expect_gte(MinHeightRel, 0)
+  expect_gte(MaxHeightRel, MinHeightRel)
+  expect_lte(MaxHeightRel, 1)
+  expect_equal(MeanHeightRel, mean(MinHeightRel, MaxHeightRel))
+  expect_equal(HeightBreadth, MaxHeightRel - MinHeightRel)
+
 })
+
+test_that("Growth rate satisfies its equation", {
+
+  sp_params <- ref_sp_params
+  sp_params$AgeAtMaturityDevCorr <- 0 # no variation in age at maturity
+
+  exptd_age_maturity <- sp_params$InterceptAgeMaturity *
+    (sp_params$MaxMass^sp_params$ScalingAgeMaturity)
+
+  species_traits <- draw_species_traits(sp_params)
+
+  exptd_mass_mat <- species_traits$MaximumMass *
+    (1 - exp(-species_traits$GrowthRate * exptd_age_maturity))
+
+  expect_equal(species_traits$MassAtMaturity, exptd_mass_mat)
+})
+
