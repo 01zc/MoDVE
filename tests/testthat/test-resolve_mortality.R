3916249808884696e14dc3da01c1f8ccbb94da0c
Theo Pannetier <theo@pannetier.nl>
2026-01-28 14:43:03 +0100

    completed unit tests for mortality

--- tests/testthat/test-resolve_mortality.R
+++ tests/testthat/test-resolve_mortality.R
@@ -1,18 +1,15 @@
-source("../tests/test-utils.R")
+source("../test-utils.R")
 
-test_that("multiplication works", {
+test_that("Mortality meets requirements", {
+
+  nb_inds <- 5000
+  rnd_mass <- runif(1, 1, 100)
 
   # Set Microhabitat matrix
-  dimensions <- c(5, 4, 2)
-  surf_area_loss <- rep(0, prod(dimensions))
+  dimensions <- sample(1:5, 3, replace = TRUE)
+  sa_loss_layer <- rep(0, prod(dimensions))
   Microhabitat <- array(0, dim = c(dimensions, 3))
   # Mortality doesn't check actual surface area so we don't need to set it
-
-  # Set surface area loss > 0 for two random cells
-  rnd_sa_loss_vox <- sample(1:prod(dimensions), 2)
-  rand_sa_loss <- sort(runif(2, 0, 1))
-  surf_area_loss[rnd_sa_loss_vox] <- rand_sa_loss
-  Microhabitat[,,,2] <- surf_area_loss
 
   # Set light niche
   SpeciesPool <- tibble::tibble(
@@ -21,28 +18,93 @@ test_that("multiplication works", {
     "MaxLight" = MinLight * runif(1, 1.05, 2)
   )
   OptimumLight <- (SpeciesPool$MinLight + SpeciesPool$MaxLight) / 2
-  light_layer <- rep(OptimumLight, prod(dimensions))
+  Microhabitat[,,,3] <- OptimumLight
 
-  # Insufficient light conditions in some voxels
-  rand_poor_light_vox <- sample(1:prod(dimensions)[-rnd_sa_loss_vox], 2)
+  # If no SA loss / light issue, death tally corresponds to mortality rate
+  MortRateRandom <- runif(1, 0, 1)
+  nb_inds <- 10000
+  E_initial <- tibble::tibble(
+    "X" = sample(1:dimensions[1], nb_inds, replace = TRUE),
+    "Y" = sample(1:dimensions[2], nb_inds, replace = TRUE),
+    "Z" = sample(1:dimensions[3], nb_inds, replace = TRUE),
+    "Status" = rep(1, nb_inds),
+    "SpeciesID" = rep(1, nb_inds),
+    "Mass" = rnd_mass
+  )
+  E <- resolve_mortality(
+    E_initial,
+    SpeciesPool,
+    Microhabitat,
+    use_mass_dep_mortality = FALSE,
+    MortRateRandom = MortRateRandom,
+    MortRateMass = 0,
+    MortRateMassScaling = 0
+  )
+  prop_dead <- sum(E$Status == 5) / nb_inds
+  expect_equal(prop_dead, MortRateRandom, tolerance = 0.02)
+
+  # When mass-dependent mortality is enabled,
+  # death tally is conform to mass-dependence equation
+  MortRateMass <- runif(1, 0, 1)
+  MortRateMassScaling <- -rgamma(1, 5, 5)
+  exptd_mortality_rate <- rnd_mass^MortRateMassScaling * MortRateMass
+  E <- resolve_mortality(
+    E_initial,
+    SpeciesPool,
+    Microhabitat,
+    use_mass_dep_mortality = TRUE,
+    MortRateRandom = 0,
+    MortRateMass = MortRateMass,
+    MortRateMassScaling = MortRateMassScaling
+  )
+  prop_dead <- sum(E$Status == 5) / nb_inds
+  expect_equal(prop_dead, exptd_mortality_rate, tolerance = 0.03)
+
+  # Individuals who are already dead are not affected by mortality
+  E <- E_initial
+  E$Status <- sample(2:5, nrow(E), replace = TRUE)
+  status_before <- E$Status
+  E <- resolve_mortality(
+    E,
+    SpeciesPool,
+    Microhabitat,
+    use_mass_dep_mortality = FALSE,
+    MortRateRandom = MortRateRandom,
+    MortRateMass = 0,
+    MortRateMassScaling = 0
+  )
+  status_after <- E$Status
+  expect_equal(status_before, status_after)
+
+  # Mortality from surface area loss and light conditions
+
+  # Set surface area loss > 0 for two random cells
+  rnd_sa_loss_vox <- sample(1:prod(dimensions), 2)
+  rand_sa_loss <- sort(runif(2, 0, 1))
+  sa_loss_layer[rnd_sa_loss_vox] <- rand_sa_loss
+  Microhabitat[,,,2] <- sa_loss_layer
+
+  # One of the two voxels with SA loss also has insufficient light
+  light_layer <- rep(OptimumLight, prod(dimensions))
+  rand_poor_light_vox <- rnd_sa_loss_vox[sample(1:2, 1)]
   light_layer[rand_poor_light_vox] <- SpeciesPool$MinLight * 0.8
   Microhabitat[,,,3] <- light_layer
 
   voxels_sa_loss <- arrayInd(which(Microhabitat[,,, 2] > 0), c(dimensions))
+  voxel_insufficient_light <- arrayInd(which(Microhabitat[,,, 3] < SpeciesPool$MinLight), c(dimensions))
 
-  nb_inds <- 5000
-  E_initial <- tibble::tibble(
+  E <- tibble::tibble(
     # Put 1000 individuals in each voxel with some SA loss
     "X" = c(rep(voxels_sa_loss[1, 1], nb_inds), rep(voxels_sa_loss[2, 1], nb_inds)),
     "Y" = c(rep(voxels_sa_loss[1, 2], nb_inds), rep(voxels_sa_loss[2, 2], nb_inds)),
     "Z" = c(rep(voxels_sa_loss[1, 3], nb_inds), rep(voxels_sa_loss[2, 3], nb_inds)),
     "Status" = rep(1, nb_inds * 2),
     "SpeciesID" = rep(1, nb_inds * 2),
-    "Mass" = runif(nb_inds * 2, 1, 100)
+    "Mass" = rnd_mass
   )
 
   E <- resolve_mortality(
-    E_initial,
+    E,
     SpeciesPool,
     Microhabitat,
     use_mass_dep_mortality = FALSE,
@@ -53,50 +115,38 @@ test_that("multiplication works", {
 
   prop_dead_branch_fall <- E |> dplyr::group_by(X, Y, Z) |>
     dplyr::filter(Status == 3) |>
-    dplyr::summarise("prop_dead_branch_fall" = dplyr::n() / nb_inds) |>
+    dplyr::summarise(
+      "prop_dead_branch_fall" = dplyr::n() / nb_inds,
+      .groups = "keep"
+      ) |>
     dplyr::pull(prop_dead_branch_fall) |> sort()
 
   # Deaths to branch fall match proportion of surface area loss
   expect_equal(prop_dead_branch_fall, rand_sa_loss, tolerance = 0.03)
 
-  E |> dplyr::filter()
+  remaining_individuals <- E |> dplyr::filter(Status != 3) |>
+    dplyr::mutate(
+      "not_enough_light" =
+        X == voxel_insufficient_light[,1] &
+        Y == voxel_insufficient_light[,2] &
+        Z == voxel_insufficient_light[,3]
+    )
 
-  E <- resolve_mortality(
-    E_initial,
-    SpeciesPool,
-    Microhabitat,
-    use_mass_dep_mortality = FALSE,
-    MortRateRandom = 0,
-    MortRateMass = 0,
-    MortRateMassScaling = 0
-  )
+  # All other individuals in voxel with not enough light die from it
+  # but surface area loss takes precedence as a source of mortality
+  all_dead_from_light <- remaining_individuals |>
+    dplyr::filter(not_enough_light) |>
+    dplyr::pull(Status) |>
+    magrittr::equals(4) |>
+    all()
+  expect_true(all_dead_from_light)
 
-  # Add dead individuals to make sure they don't die twice
-  nb_already_dead <- 100
-  E <- E |> dplyr::bind_rows(tibble::tibble(
-    "X" = rep(voxels_sa_loss[1, 1], nb_already_dead),
-    "Y" = rep(voxels_sa_loss[1, 2], nb_already_dead),
-    "Z" = rep(voxels_sa_loss[1, 3], nb_already_dead),
-    # Status 2 to 5 correspond to dead individuals, and 3 to death to branch loss
-    "Status" = sample(c(2, 4, 5), nb_already_dead, replace = TRUE),
-    "SpeciesID" = rep(1, nb_already_dead),
-    "Mass" = runif(nb_already_dead, 1, 100)
-  ))
-
-
-  # for voxels where light < MinLight or > MaxLight all individuals status 4
-
-  # sa_loss mortality takes precedence over other forms of mortality
-  # if light out of conditions and sa_loss > 0
-  # then fro 1000 individuals nb status 4 = (1-sa_loss)
-
-  # assuming sa_loss 0 and !use_mass_dep_mortality
-  # then nb status 5 = MortRateRandom
-
-  # if use_mass_dep_mortality
-  # Mass^MortRateMassScaling * MortRateMass
-
-
-  # individuals with status > 1 are not affected by death
+  # All remaining individuals in voxel with enough light are still alive
+  all_alive <- remaining_individuals |>
+    dplyr::filter(!not_enough_light) |>
+    dplyr::pull(Status) |>
+    magrittr::equals(1) |>
+    all()
+  expect_true(all_alive)
 
 })
