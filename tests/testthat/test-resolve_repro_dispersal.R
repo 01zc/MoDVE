859cf38d1f6bb8f6ca3e5d4e8d18280691dd179e
Theo Pannetier <theo@pannetier.nl>
2026-02-12 18:47:55 +0100

    fixed discrepancy between versions

--- tests/testthat/test-resolve_repro_dispersal.R
+++ tests/testthat/test-resolve_repro_dispersal.R
@@ -1,7 +1,6 @@
-source("tests/old_funcs.R")
-source("tests/test-utils.R")
-
-species_params <- parse_config("tests/config_a2.toml")
+source("../old_funcs.R")
+source("../test-utils.R")
+species_params <- parse_config("../config_a2.toml")
 
 test_that("consistent with old version", {
 
@@ -22,63 +21,37 @@ test_that("consistent with old version", {
     SpeciesPool,
     Microhabitat
   )
-
-  # nb_inds <- sample(1:dimensions[1], 1)
-  # nb_inds <- 20
-  # E <- tibble::tibble(
-  #  SpeciesID = 1,
-  #  IndividualID = seq_len(nb_inds),
-  #  Status = 1,
-  #  # Initialise individuals randomly
-  #  X = rep(sample(dimensions[1], nb_inds, replace = TRUE)),
-  #  Y = rep(sample(dimensions[2], nb_inds, replace = TRUE)),
-  #  Z = rep(sample(dimensions[3], nb_inds, replace = TRUE)),
-  #  Mass = runif(nb_inds, 0, 1),
-  #  MassAtMaturity = Mass,
-  #  MaxMass = Mass,
-  #  RecruitmentInvestmentRel = runif(nb_inds, 0, 1),
-  #  # Compute expected surface area
-  #  expected_sa = Mass^(2 / 3) / SurfaceBiomassScaling
-  #)
-
-  # E[, c("TotalSurfaceInVoxel", "LightInVoxel", "SurfaceLossInVoxel")] <- 0
-
-  prob_disp_matrix <- calc_prob_disp_matrix(centralPoint,
-                                            dimensions[1],
-                                            dimensions[2],
-                                            dimensions[3],
-                                            SpeciesPool)
-
   max_id <- max(E$IndividualID)
-  InterceptRecruitment <- runif(0, 100, 1)
-  SlopeRecruitment <- runif(0, 1, 1)
+
+  InterceptRecruitment <- runif(1, 0, 100)
+  SlopeRecruitment <- runif(1, 0, 1)
 
   # Format input to the old version
-  dimX <- dimensions[1] * 2 + 1
-  dimY <- dimensions[2] * 2 + 1
-  dimZ <- dimensions[3] * 2 + 1
-  central_point_old <- c(
-    floor(dimX/2) + 1,
-    floor(dimY/2) + 1,
-    floor(dimZ/2) + 1
+  dims_with_corr <- dimPlot <-  dimensions * 2 + 1
+  ProbabilityMatrixNormalized <- old_compute_prob_matrix_norm(
+    centralPoint,
+    dims_with_corr[1],
+    dims_with_corr[2],
+    dims_with_corr[3],
+    nb_species,
+    SpeciesPool
   )
-  prob_disp_matrix_old <- old_compute_prob_matrix_norm(
-    central_point_old, dimX, dimY, dimZ, nb_species, SpeciesPool
-    )
-  E_old <- E
-  E_old[, (ncol(E)+1):(ncol(E)+1+ncol(SpeciesPool))] <- SpeciesPool
-
-
+  E[, (ncol(E)+1):(ncol(E)+1+ncol(SpeciesPool))] <- SpeciesPool
+  {
+    NumberOfSpecies <- nb_species
+    dimPlot <- dims_with_corr
+    MaxIndividualID <- max_id
+  }
   disp_list <- old_dispersal(
     nb_species,
-    E_old,
+    E,
     Microhabitat,
     SurfaceBiomassScaling,
     dimensions,
-    central_point_old,
+    centralPoint,
     InterceptRecruitment,
     SlopeRecruitment,
-    prob_disp_matrix,
+    ProbabilityMatrixNormalized,
     SpeciesPool,
     max_id
   )
@@ -94,6 +67,13 @@ test_that("consistent with old version", {
     "recruitment_df" = recruitment_df,
     "max_id" = disp_list$MaxIndividualID
   )
+
+  prob_disp_matrix <- calc_prob_disp_matrix(centralPoint,
+                                            dimensions[1],
+                                            dimensions[2],
+                                            dimensions[3],
+                                            SpeciesPool)
+  prob_disp_matrix <- ProbabilityMatrixNormalized
 
   res_obs <- resolve_repro_dispersal(
     E,
